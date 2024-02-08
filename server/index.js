150dfe6b1ecce73046fe36f7a2ce5e05afbc46f0
Suhanii1214 <suhani.12010153@lpu.in>
2024-02-08 05:37:55 +0530

    completed

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import cors from 'cors'
+import ENV from './config.js'
 import { router as authRouter } from './routes/auth.js'
 import Razorpay from 'razorpay'
 import { itemRouter } from './routes/item.js'
@@ -7,6 +9,9 @@ import { orderRouter } from './routes/order.js'
 import { cartRouter } from './routes/cart.js'
 
 const app  = express()
+
+app.use(cors())
+
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
@@ -17,6 +22,6 @@ app.use('/api', cartRouter)
 
 // connecting to the database
 const port = 4000
-mongoose.connect("mongodb+srv://suhani:Ss1234@ecommerce.4zgrk4o.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(ENV.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => app.listen(port, () => console.log(`Server running on port ${port}`)))
 .catch((err) => console.log(err))
